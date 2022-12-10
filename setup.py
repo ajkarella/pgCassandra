7f96aac38816910a1be4a46ef385766ac8310388
Ambrose Karella <ajkarella@gmail.com>
2022-12-10 02:46:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import subprocess
 from setuptools import setup, find_packages, Extension
 
 setup(
-  name='pgCassandra2',
+  name='pgCassandra',
   version='0.9.2',
   author='Wojciech Krol',
   link = 'https://github.com/ajkarella/pgCassandra/',
   license='Postgresql',
-  packages=['pgCassandra2']
+  packages=['pgCassandra']
 )
